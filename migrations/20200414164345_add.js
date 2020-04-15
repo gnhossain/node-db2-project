4cf9c4142d44448440f4b87ebb06763e010a17d2
gul hossain <spicymangojungle@gmail.com>
2020-04-15 13:45:57 -0700

    update migration

--- migrations/20200414164345_add.js
+++ migrations/20200414164345_add.js
@@ -1,8 +1,19 @@
 
 exports.up = function(knex) {
-  
+    // the change we want to make to our schema
+  return knex.schema.createTable('car', tbl => {
+      tbl.increments()
+      tbl.string('VIN').unique().notNullable()
+      tbl.string('Make').notNullable()
+      tbl.string('Model').notNullable()
+      tbl.integer('Mileage').notNullable()
+      tbl.string('Transmission')
+      tbl.string('Title')
+  });
 };
 
 exports.down = function(knex) {
+    // undoing that change
+    return knex.schema.dropTableIfExists('cars')
   
 };
